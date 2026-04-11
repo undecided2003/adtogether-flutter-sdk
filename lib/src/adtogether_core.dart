7d1b6efac18bfbeb383d8eabff571aafc0f18795
DRBAapp <relaxpropertiesllc@gmail.com>
2026-04-11 14:01:58 -0700

    fix: ensure apiKey is sent in tracking events across all SDKs for credit tracking

--- lib/src/adtogether_core.dart
+++ lib/src/adtogether_core.dart
@@ -37,6 +37,7 @@ class AdTogether {
         body: jsonEncode({
           'adId': adId,
           if (token != null) 'token': token,
+          'apiKey': _appId,
         }),
       );
     } catch (e) {
@@ -53,6 +54,7 @@ class AdTogether {
         body: jsonEncode({
           'adId': adId,
           if (token != null) 'token': token,
+          'apiKey': _appId,
         }),
       );
     } catch (e) {
